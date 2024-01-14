db65ecfa948622d539e5aae8636d3ae3af95c670
davidolejniczak <David.olejniczak@icloud.com>
2024-01-14 18:40:56 -0500

    Made walking skeleton of the code

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -17,6 +17,7 @@ public class Main {
                 logger.info("**** Reading the maze from file " + args[1]);
                 BufferedReader reader = new BufferedReader(new FileReader(args[1]));
                 String line;
+                mazemaker(args[1]);
                 while ((line = reader.readLine()) != null) {
                     for (int idx = 0; idx < line.length(); idx++) {
                         if (line.charAt(idx) == '#') {
@@ -28,12 +29,45 @@ public class Main {
                     System.out.print(System.lineSeparator());
                 }
             }
+            if ("-p".equals(args[2])) {
+                String userpath = pathCanonicalMaker(args[3]);//code to input an factorized path from the command line to canonical
+                pathCheck(userpath); //return if path is correct or not, add extra input that is the maze file or somthing that can repersent it
+
+            }
         } catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
         }
-        logger.info("**** Computing path");
-        logger.info("PATH NOT COMPUTED");
-        logger.info("** End of MazeRunner");
+        if (!("-p".equals(args[2]))) {
+            logger.info("**** Computing path");
+            try {
+                String solvedpath = pathFinder(args[1]);//return path in canonical form
+                pathFactorizedPrinter(solvedpath); //prints path in factorized form
+            } catch(Exception e) {
+                logger.info("PATH NOT COMPUTED");
+            }
+            logger.info("** End of MazeRunner");
+        }
+    }
 
+    public static void mazeMaker() {
+        //take the input maze and create a matrix that will store the maze so that it can be used in other functions
+    }
+    public static void pathCheck(String userpath){
+        //checks if user path will work in maze
+        //prints result
+    }
+
+    public static String pathCanonicalMaker(String userpathfactored){
+        //converts the users factored path to canonical path that the computer can use to check if its correct
+    }
+
+    public static String pathFinder(String filepath){
+        //finds the correct path throw the maze using a right hand strat while recording all moves taken
+    }
+
+    public static void pathFactorizedPrinter(String solvedpath){
+        //takes in the solved path for a maze in canonical form and converts it too factorized form
+        //prints result
     }
 }
+
