8af15c3f91ebad941bd74e94000050e091cbe5c9
davidolejniczak <David.olejniczak@icloud.com>
2024-01-20 22:13:02 -0500

    Maze storage method working  (close #2)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -5,22 +5,21 @@ import java.io.IOException;
 
 public class Maze {
     public static void mazeMaker(String filename) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        BufferedReader readerCount = new BufferedReader(new FileReader(filename));
         String lineCountLoop;
         int mazeRows = 0,mazeColn = 0;
-        while ((lineCountLoop = reader.readLine()) != null) {
+        while ((lineCountLoop = readerCount.readLine()) != null) {
             mazeRows =  lineCountLoop.length();
             for (int index = 0; index < lineCountLoop.length(); index++) {
                 mazeColn = index;
             }
         }
-
-        int[][] mazeStorage = new int[mazeRows][mazeColn];
-
+        int[][] mazeStorage = new int[mazeRows][mazeColn+1];
+        BufferedReader readerPrint = new BufferedReader(new FileReader(filename));
         String linePrintLoop;
         int rowCount = 0;
-        while ((linePrintLoop = reader.readLine()) != null) {
-            for (int index = 0; index < linePrintLoop.length(); index++, rowCount++) {
+        while ((linePrintLoop = readerPrint.readLine()) != null) {
+            for (int index = 0; index < linePrintLoop.length(); index++) {
                 if (linePrintLoop.charAt(index) == '#') {
                     System.out.print("WALL ");
                     mazeStorage[rowCount][index] = 1;
@@ -29,7 +28,9 @@ public class Maze {
                     mazeStorage[rowCount][index] = 0;
                 }
             }
+            rowCount++;
             System.out.print(System.lineSeparator());
         }
     }
-}
+    }
+
