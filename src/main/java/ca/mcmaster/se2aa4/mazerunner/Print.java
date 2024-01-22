cc493ca6ff8f6b3934177c2c500048a44358ca62
davidolejniczak <David.olejniczak@icloud.com>
2024-01-22 14:52:34 -0500

    Added a method to turn a factored path to a canonical (close #5)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Print.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Print.java
@@ -1,10 +1,37 @@
 package src.main.java.ca.mcmaster.se2aa4.mazerunner;
 
 public class Print {
+    private static char currentChar;
+    private static char currentCharWithIndex;
 
     public static String pathCanonicalMaker(String userpathfactored){
-        //converts the users factored path to canonical path that the computer can use to check if its correct
         StringBuilder factoredPath = new StringBuilder(" ");
+        for (int index=0; index < userpathfactored.length();index++) {
+            char currentChar = userpathfactored.charAt(index);
+            if ("R".equals(currentChar) || "F".equals(currentChar) || "L".equals(currentChar)) {
+                factoredPath.append((currentChar));
+            }
+            else if (currentChar >= '0' && currentChar <= '9') {
+                int numOfSpace = Integer.parseInt(Character.toString(currentChar));
+                index++;
+                char currentCharNum = userpathfactored.charAt(index);
+                if ("R".equals(currentCharNum)) {
+                    for (int n=0; n<numOfSpace; n++){
+                        factoredPath.append(currentCharNum);
+                    }
+                }
+                if ("F".equals(currentCharNum)) {
+                    for (int n=0; n<numOfSpace; n++){
+                        factoredPath.append(currentCharNum);
+                    }
+                }
+                if ("L".equals(currentCharNum)) {
+                    for (int n=0; n<numOfSpace; n++){
+                        factoredPath.append(currentCharNum);
+                    }
+                }
+            }
+        }
         return factoredPath.toString();
     }
 
@@ -14,6 +41,7 @@ public class Print {
         factoredPath.append("");
         return factoredPath.toString();
     }
+
 
     public static void printPath(String solvedPath) {
         System.out.println("Starting from the west side of the maze");
